91ef1e4d6f218858fe5a1656e109c8d57213537e
ChatcharitCh <chatcharit.choocherd@gmail.com>
2023-06-27 16:46:47 +0700

    Create Controller

---
+++ src/main/java/com/project/countriesapi/CountryController.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.project.countriesapi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author Chatcharit
+ */
+@RestController
+public class CountryController {
+    
+    @RequestMapping("/hello")
+    @ResponseBody
+    private String hello(){
+        return "Hello World";
+    }
+    
+    @RequestMapping("/country")
+    @ResponseBody
+    private String getCountry(){
+        
+        String uri = "https://topups.reloadly.com/countries/TH";
+        RestTemplate restTemplate = new RestTemplate();
+        
+        Country country = restTemplate.getForObject(uri, Country.class);
+        System.out.println("Country" + country);
+        
+        System.out.println("isoName = " + country.getIsoName());
+        System.out.println("name = " + country.getName());
+        System.out.println("continent = " + country.getContinent());
+        System.out.println("currencyCode = " + country.getCurrencyCode());
+        System.out.println("currencyName = " + country.getCurrencyName());
+        System.out.println("currencySymbol = " + country.getCurrencySymbol());
+        System.out.println("flag = " + country.getFlag());
+        System.out.println("callingCodes = " + country.getCallingCodes());
+        
+        return "Contry detail page.";
+    }
+}
